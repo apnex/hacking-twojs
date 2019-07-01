51404432018dd5dbce099ea98c965f9f37f64c14
root <aobersnel@apnex.com.au>
2019-07-01 15:31:41 +1000

    Updates...

--- link/main.js
+++ link/main.js
@@ -8,13 +8,13 @@ const a = anchor();
 
 // get lb
 let lb = loadbalancer();
-lb.scale = 0.2;
+lb.scale = 0.4;
 let rt = router();
-rt.scale = 0.2;
+rt.scale = 0.4;
 let sw = netswitch();
-sw.scale = 0.2;
+sw.scale = 0.4;
 let fw = firewall();
-fw.scale = 0.2;
+fw.scale = 0.4;
 
 
 // path1
@@ -35,17 +35,19 @@ aGrid([
 	[4,0,0,0,3]
 ]);
 aGrid.painter(aPainter);
-aGrid.x(40);
-aGrid.y(40);
+aGrid.x(80);
+aGrid.y(80);
 aGrid.offset(aGrid.get(9)); // obtain center coord
 //aGrid.center(9);
 
-let sym = new Two.Group();
-//sym.add(aGrid.show()); // doesnt work - needs offset applied
+// set main
+let main = new Two.Group();
+main.translation.set(two.width / 2, two.height / 2);
+//main.add(aGrid.show()); // doesnt work - needs offset applied
 [
 	[1, 2, 3, 4]
 ].forEach((link) => {
-	sym.add(aGrid.addPath(link, {
+	main.add(aGrid.addPath(link, {
 		close: 1,
 		radius: 40
 		//start: lb,
@@ -54,40 +56,20 @@ let sym = new Two.Group();
 });
 
 let icons = new Two.Group();
-[1, 3].forEach((tag) => {
-	let p = aGrid.get(tag); // obtain center coord
-	let px = aGrid.at(p.x, p.y); // relative to offset!!
-	let moo = lb.clone();
-	moo.translation.set(px.x, px.y);
-	icons.add(moo);
-});
-[2].forEach((tag) => {
-	let p = aGrid.get(tag); // obtain center coord
-	let px = aGrid.at(p.x, p.y); // relative to offset!!
-	let moo = rt.clone();
-	moo.translation.set(px.x, px.y);
-	icons.add(moo);
-});
-[3].forEach((tag) => {
-	let p = aGrid.get(tag); // obtain center coord
-	let px = aGrid.at(p.x, p.y); // relative to offset!!
-	let moo = sw.clone();
-	moo.translation.set(px.x, px.y);
-	icons.add(moo);
-});
-[4].forEach((tag) => {
-	let p = aGrid.get(tag); // obtain center coord
-	let px = aGrid.at(p.x, p.y); // relative to offset!!
-	let moo = fw.clone();
-	moo.translation.set(px.x, px.y);
-	icons.add(moo);
-});
+function placeIcon(icon, tags) {
+	tags.forEach((tag) => {
+		let p = aGrid.get(tag);
+		let px = aGrid.at(p.x, p.y);
+		let symbol = icon.clone();
+		symbol.translation.set(px.x, px.y);
+		icons.add(symbol);
+	});
+}
+placeIcon(lb, [1]);
+placeIcon(sw, [2]);
+placeIcon(fw, [3]);
+placeIcon(rt, [4]);
 
-// set main
-let main = new Two.Group();
-main.translation.set(two.width / 2, two.height / 2);
-
-main.add(sym);
 main.add(icons);
 two.add(main);
 
